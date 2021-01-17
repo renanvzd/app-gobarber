db2f6b2014ca0d4973eb36147aa21571f61a9cc9
Renan Veronez Drechsler <renanvz@outlook.com>
2021-01-17 18:07:11 -0300

    Autenticaçao de usuario

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,93 @@
+import React, {
+  createContext,
+  useCallback,
+  useState,
+  useContext,
+  useEffect,
+} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  userWithoutPassword: object;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  userWithoutPassword: object;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>({} as AuthState);
+
+  useEffect(() => {
+    async function loadStoragedData(): Promise<void> {
+      const [token, userWithoutPassword] = await AsyncStorage.multiGet([
+        '@GoBarber:token',
+        '@GoBarber:userWithoutPassword',
+      ]);
+
+      if (token[1] && userWithoutPassword[1]) {
+        setData({
+          token: token[1],
+          userWithoutPassword: JSON.parse(userWithoutPassword[1]),
+        });
+      }
+    }
+    loadStoragedData();
+  }, []);
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', {
+      email,
+      password,
+    });
+
+    const { token, userWithoutPassword } = response.data;
+
+    await AsyncStorage.multiSet([
+      ['@GoBarber:token', token],
+      ['@GoBarber:userWithoutPassword', JSON.stringify(userWithoutPassword)],
+    ]);
+
+    setData({ token, userWithoutPassword });
+  }, []);
+
+  const signOut = useCallback(async () => {
+    await AsyncStorage.multiRemove([
+      '@GoBarber:token',
+      '@GoBarber:userWithoutPassword',
+    ]);
+
+    setData({} as AuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ userWithoutPassword: data.userWithoutPassword, signIn, signOut }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
